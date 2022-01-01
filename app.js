658488dfcb32f1d738367313264701fe3dcb3727
Stefanos Dianellos <stefanosdianellos@gmail.com>
2022-01-01 12:11:10 +0200

    Add basic structure

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const mongoSanitize = require("express-mongo-sanitize");
+
+const sailorRoutes = require("./routes/sailors");
+
+const MongoDBStore = require("connect-mongo")(session);
+
+dbUrl = "mongodb://127.0.0.1:27017/Navy";
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize({
+    replaceWith: "_"
+}));
+
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
+
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+});
+
+const sessionConfig = {
+    store,
+    name: "session",
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
+
+app.use("/lighthouse", sailorRoutes);
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+app.listen(3000, () => {
+    console.log("Listening in port 3000");
+});
---
+++ routes/sailors.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
+
+module.exports = router;
