bc021d22426534d9700fabe57f6af98948c331b4
Stefanos Dianellos <stefanosdianellos@gmail.com>
2022-02-25 10:00:32 +0200

    Add update functionality

--- routes/lightbeacons.js
+++ routes/lightbeacons.js
@@ -92,4 +92,56 @@ router.get("/insertLight/:id", catchAsync(async (req, res) => {
 	res.render("insertLight", { lightBeacon });
 }));
 
+router.put("/insertLight/:id", catchAsync(async (req, res) => {
+	try {
+		var id;
+		const tmp = req.params.id.charAt(0);
+		if(tmp === ":"){
+			id = req.params.id.substring(1);
+		}
+		else{
+			id = req.params.id;
+		}
+		const lightBeacon = await LightBeacon.findByIdAndUpdate(id, { ...req.body});
+				
+		lightBeacon.stigma.x = req.body.stigmax;
+		lightBeacon.stigma.y = req.body.stigmax;
+		lightBeacon.lightingMachine.manufacturer = req.body.lightingMachineMan;
+		lightBeacon.lightingMachine.type = req.body.lightingMachineType;
+		lightBeacon.lightingMachine.counter = req.body.lightingMachineCounter;
+		lightBeacon.reflector.manufacturer = req.body.reflectorMan;
+		lightBeacon.reflector.type = req.body.reflectorType;
+		lightBeacon.reflector.counter = req.body.reflectorCounter;
+		lightBeacon.signs.manufacturer = req.body.signsMan;
+		lightBeacon.signs.type = req.body.signsType;
+		lightBeacon.signs.counter = req.body.signsCounter;
+		lightBeacon.stateri.type = req.body.stateriType;
+		lightBeacon.stateri.counter = req.body.stateriCounter;
+		lightBeacon.alysos.type = req.body.alysosType;
+		lightBeacon.alysos.counter = req.body.alysosCounter;
+		lightBeacon.agkyrioChain.type = req.body.agkyrioChainType;
+		lightBeacon.agkyrioChain.counter = req.body.agkyrioChainCounter;
+		lightBeacon.streptyras.type = req.body.streptyrasType;
+		lightBeacon.streptyras.counter = req.body.streptyrasCounter;
+		lightBeacon.navyKey.type = req.body.navyKeyType;
+		lightBeacon.navyKey.counter = req.body.navyKeyCounter;
+		lightBeacon.navyKeyV.type = req.body.navyKeyVType;
+		lightBeacon.navyKeyV.counter = req.body.navyKeyVCounter;
+		lightBeacon.agkyrio.type = req.body.agkyrioType;
+		lightBeacon.agkyrio.counter = req.body.agkyrioCounter;
+
+		moment.locale('el');
+		lightBeacon.dateModified = moment().format('LL');
+		
+		await lightBeacon.save();
+	}
+	catch(e) {
+		req.flash("error", e.message);
+		res.redirect('/lightBeacons/insertConstant/:id');
+	}
+	
+    req.flash("success", "Επιτυχής ενημέρωση!");
+	res.redirect(`/lightBeacons/technicians/${req.params.id}`);		
+}));
+
 module.exports = router;
