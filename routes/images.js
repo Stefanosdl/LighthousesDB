e7f96eaf158e54d680d2e93be5e78621f9f08b5a
Stefanos Dianellos <stefanosdianellos@gmail.com>
2022-03-22 14:03:35 +0200

    WIP Add correct paths to photos

--- routes/images.js
+++ routes/images.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const LedLight = require("../models/ledlighthouses");
@@ -45,17 +46,40 @@ router.get("/search", catchAsync(async (req, res, next) => {
     try {
         const query = req.query.q;
         if (query) {
-            const searchedAuto = await AutoLight.find({ $or:[ {aef: query}, {lighthouse: query}, {location: query} ]}).populate("technicians");
-            const searchedLed = await LedLight.find({ $or:[ {aef: query}, {lighthouse: query}, {location: query} ]}).populate("technicians");
-            const searchedConstant = await ConstantLight.find({ $or:[ {aef: query}, {lighthouse: query}, {location: query} ]}).populate("technicians");
-            const searchedLight = await LightBeacon.find({ $or:[ {aef: query}, {lighthouse: query}, {location: query} ]}).populate("technicians");
+            const searchedAuto = await AutoLight.findOne({aef: query}).populate("technicians");
+            const searchedLed = await LedLight.findOne({aef: query}).populate("technicians");
+            const searchedConstant = await ConstantLight.findOne({aef: query}).populate("technicians");
+            const searchedLight = await LightBeacon.findOne({aef: query}).populate("technicians");
+            
+            const autoIm = new Array();
+            const ledIm = new Array();
+            const constantIm = new Array();
+            const lightIm = new Array();
+            var tmp = "";
+
+            fs.readdirSync("C:/Users/release-builds/LighthousesDB-win32-ia32/photos/<%= searchedAuto.aef %>").forEach((file) => {
+                tmp = "C:/Users/release-builds/LighthousesDB-win32-ia32/photos/<%= searchedAuto.aef %>/".concat(file);
+                autoIm.push(tmp)
+            });
+            fs.readdirSync("C:/Users/release-builds/LighthousesDB-win32-ia32/photos/<%= searchedLed.aef %>").forEach((file) => {
+                tmp = "C:/Users/release-builds/LighthousesDB-win32-ia32/photos/<%= searchedLed.aef %>/".concat(file);
+                ledIm.push(tmp)
+            });
+            fs.readdirSync("C:/Users/release-builds/LighthousesDB-win32-ia32/photos/<%= searchedConstant.aef %>").forEach((file) => {
+                tmp = "C:/Users/release-builds/LighthousesDB-win32-ia32/photos/<%= searchedConstant.aef %>/".concat(file);
+                constantIm.push(tmp)
+            });
+            fs.readdirSync("C:/Users/release-builds/LighthousesDB-win32-ia32/photos/<%= searchedLight.aef %>").forEach((file) => {
+                tmp = "C:/Users/release-builds/LighthousesDB-win32-ia32/photos/<%= searchedLight.aef %>/".concat(file);
+                lightIm.push(tmp)
+            });
 
             if((searchedAuto == undefined || searchedAuto.length == 0) && (searchedLed == undefined || searchedLed.length == 0) && (searchedConstant == undefined || searchedConstant.length == 0) && (searchedLight == undefined || searchedLight.length == 0)) {
                 req.flash("error", "Η αναζήτησή σας δεν είχε κανένα αποτέλεσμα!");
                 res.redirect('/');
             }
             else {
-                res.render("images/search", { searchedAuto, searchedLed, searchedConstant, searchedLight});
+                res.render("images/search", { autoIm, ledIm, constantIm, lightIm});
             }
         }
     }
